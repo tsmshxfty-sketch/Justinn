6c252b5b53ea586dfa1855300613b93df4381862
tsmshxfty-sketch <tsmshxfty@gmail.com>
2025-09-26 22:58:51 -0400

    Add files via upload

--- Home_Page.py
+++ Home_Page.py
@@ -3,26 +3,28 @@ import streamlit as st
 # Title of App
 st.title("Web Development Lab01")
 
-# Assignment Data 
-# TODO: Fill out your team number, section, and team members
+# Sidebar navigation
+page = st.sidebar.selectbox("Navigate", ["Home", "Portfolio", "Quiz"])
 
-st.header("CS 1301")
-st.subheader("Web Development - Section X")
-st.subheader("Name")
+# Home Page
+if page == "Home":
+    st.header("CS 1301")
+    st.subheader("Web Development - Section X")
+    st.subheader("Name")
 
+    st.write("""
+    Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
 
-# Introduction
-# TODO: Write a quick description for all of your pages in this lab below, in the form:
-#       1. **Page Name**: Description
-#       2. **Page Name**: Description
-#       3. **Page Name**: Description
-#       4. **Page Name**: Description
+    1. **Portfolio**: All about Justin Palmer  
+    2. **Quiz**: Quiz about Justin Palmer
+    """)
 
-st.write("""
-Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+# Portfolio Page
+elif page == "Portfolio":
+    st.header("Portfolio")
+    st.write("All about Justin Palmer")
 
-1. **Portfolio**: All about Justin Palmer
-2. **Quiz**: Quiz about Justin Palmer
-
-""")
-
+# Quiz Page
+elif page == "Quiz":
+    st.header("Quiz")
+    st.write("Take a quiz about Justin Palmer!")
